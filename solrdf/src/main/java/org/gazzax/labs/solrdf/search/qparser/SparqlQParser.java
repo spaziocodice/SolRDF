e8fecd77471c694d7782a2d73288976629f795b5
agazzarini <a.gazzarini@gmail.com>
2014-12-12 18:59:18 +0100

    [ issue #4 ] Javadoc on SPARQL QParser (Plugin)

--- solrdf/src/main/java/org/gazzax/labs/solrdf/search/qparser/SparqlQParser.java
+++ solrdf/src/main/java/org/gazzax/labs/solrdf/search/qparser/SparqlQParser.java
@@ -9,8 +9,23 @@ import org.apache.solr.search.SyntaxError;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.QueryParseException;
 
+/**
+ * A SPARQL Query Parser.
+ * That actually delegates the parsing logic to Jena parser.
+ * 
+ * @author Andrea gazzarini
+ * @since 1.0
+ */
 public class SparqlQParser extends QParser {
 
+	/**
+	 * Builds a new {@link QParser} with the given data.
+	 * 
+	 * @param qstr the query string.
+	 * @param localParams the local parameters in the request.
+	 * @param params the request parameters.
+	 * @param req the current Solr request.
+	 */
 	SparqlQParser(
 			final String qstr, 
 			final SolrParams localParams,
--- solrdf/src/main/java/org/gazzax/labs/solrdf/search/qparser/SparqlQParserPlugin.java
+++ solrdf/src/main/java/org/gazzax/labs/solrdf/search/qparser/SparqlQParserPlugin.java
@@ -6,8 +6,15 @@ import org.apache.solr.request.SolrQueryRequest;
 import org.apache.solr.search.QParser;
 import org.apache.solr.search.QParserPlugin;
 
+/**
+ * SPARQL {@link QParserPlugin}.
+ * Acts as a factory and configuration point for SPARQL {@link QParser}s.
+ * 
+ * @author Andrea Gazzarini
+ * @sice 1.0
+ */
 public class SparqlQParserPlugin extends QParserPlugin {
-
+	@Override
 	public void init(@SuppressWarnings("rawtypes") final NamedList args) {
 		// Nothing to be done at the momyny
 	}
