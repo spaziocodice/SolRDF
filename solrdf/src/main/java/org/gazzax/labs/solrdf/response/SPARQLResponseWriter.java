ad6116c196b59055cb31cf463ac6b3578a451d5c
agazzarini <a.gazzarini@gmail.com>
2014-12-14 18:40:54 +0100

    [ issue #9 ] Composite response writer for supporting ASK, SELECT, DESCRIBE, CONSTRUCT queries

--- solrdf/src/main/java/org/gazzax/labs/solrdf/response/SPARQLResponseWriter.java
+++ solrdf/src/main/java/org/gazzax/labs/solrdf/response/SPARQLResponseWriter.java
@@ -2,10 +2,13 @@ package org.gazzax.labs.solrdf.response;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.http.HttpHeaders;
+import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.riot.RDFLanguages;
 import org.apache.jena.riot.ResultSetMgr;
 import org.apache.solr.common.util.NamedList;
@@ -19,6 +22,8 @@ import org.restlet.engine.io.WriterOutputStream;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecution;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
 
 /**
  * Formats a SPARQL result.
@@ -28,13 +33,27 @@ import com.hp.hpl.jena.query.ResultSet;
  * @since 1.0
  */
 public class SPARQLResponseWriter implements QueryResponseWriter {
+	private static abstract interface ResponseWriter {
+		/**
+		 * Writes out the response.
+		 * 
+		 * @param response the response value object.
+		 * @param writer the output writer.
+		 * @param execution the query execution.
+		 * @param contentType determines the output format.
+		 * @throws IOException in case of I/O failure-
+		 */
+		@SuppressWarnings("rawtypes")
+		abstract void doWrite(NamedList response, Writer writer, QueryExecution execution, String contentType) throws IOException;
+	}
+
+	private Map<Integer, ResponseWriter> writers = new HashMap<Integer, ResponseWriter>();  
 	
 	@Override
 	public void write(
 			final Writer writer, 
 			final SolrQueryRequest request, 
-			final SolrQueryResponse response) throws IOException {
-		
+			final SolrQueryResponse response) throws IOException {		
 		final HttpServletRequest httpRequest = (HttpServletRequest) request.getContext().get("httpRequest");
 		final String accept = httpRequest.getHeader(HttpHeaders.ACCEPT);
 		
@@ -43,32 +62,96 @@ public class SPARQLResponseWriter implements QueryResponseWriter {
 		try {
 			final Query query = (Query)values.get(Names.QUERY);
 			execution = (QueryExecution) values.get(Names.QUERY_EXECUTION);
-			if (query.isSelectType()) {
-				final ResultSet resultSet = (ResultSet) values.get(Names.QUERY_RESULT);
-				if (execution != null && resultSet != null) {
-					ResultSetMgr.write(
-							new WriterOutputStream(writer, CharacterSet.UTF_8), 
-							resultSet, 
-							RDFLanguages.contentTypeToLang(accept));
-				}
+			if (query == null || execution == null) {
+				return;
 			}
+			
+			final ResponseWriter responseWriter = writers.get(query.getQueryType());
+			responseWriter.doWrite(values, writer, execution, accept);
 		} finally {
 			if (execution != null) {
 				try {execution.close();} catch (final Exception ignore) {}
 			}
-		}
+		}				
 	}
 
 	@Override
 	public String getContentType(final SolrQueryRequest request, final SolrQueryResponse response) {
-		final HttpServletRequest httpRequest = (HttpServletRequest) request.getContext().get("httpRequest");
-		final String accept = httpRequest.getHeader(HttpHeaders.ACCEPT);
-		return accept;
+		return getContentType(request);
 	}
-
+	
 	@SuppressWarnings("rawtypes")
 	@Override
 	public void init(final NamedList args) {
-		// Nothing to be done here...
+		writers.put(Query.QueryTypeAsk, new ResponseWriter() {
+			@Override
+			public void doWrite(
+					final NamedList response, 
+					final Writer writer, 
+					final QueryExecution execution, 
+					final String contentType) {
+				Boolean askResult = response.getBooleanArg(Names.QUERY_RESULT);
+				if ("text/csv".equals(contentType) || "text/plain".equals(contentType)) {
+					ResultSetFormatter.outputAsCSV(askResult);
+				} else if ("text/tab-separated-values".equals(contentType)) {
+					ResultSetFormatter.outputAsTSV(askResult);
+				} else if ("application/sparql-results+xml".equals(contentType)) {
+					ResultSetFormatter.outputAsXML(askResult);
+				} else if ("application/sparql-results+json".equals(contentType)) {
+					ResultSetFormatter.outputAsJSON(askResult);
+				} 
+			}
+		});
+
+		writers.put(Query.QueryTypeSelect, new ResponseWriter() {
+			@Override
+			public void doWrite(
+					final NamedList response, 
+					final Writer writer, 
+					final QueryExecution execution, 
+					final String contentType) {
+				final ResultSet resultSet = (ResultSet) response.get(Names.QUERY_RESULT);
+				ResultSetMgr.write(
+					new WriterOutputStream(writer, CharacterSet.UTF_8), 
+					resultSet, 
+					RDFLanguages.contentTypeToLang(contentType));
+			}
+		});
+		
+		final ResponseWriter modelResponseWriter = new ResponseWriter() {
+			@Override
+			public void doWrite(
+					final NamedList response, 
+					final Writer writer, 
+					final QueryExecution execution, 
+					final String contentType) {
+				final Model model = (Model) response.get(Names.QUERY);
+				RDFDataMgr.write(
+						new WriterOutputStream(writer, CharacterSet.UTF_8), 
+						model, 
+						RDFLanguages.contentTypeToLang(contentType));
+			}
+		};
+		
+		writers.put(Query.QueryTypeDescribe, modelResponseWriter);
+		writers.put(Query.QueryTypeConstruct, modelResponseWriter);
+	}
+	
+	/**
+	 * Determines the content type associated with this response writer.
+	 * 
+	 * The mime type is first determined using the Accept request header. 
+	 * If that is null, then the text/plain is assumed as default content type.
+	 * 
+	 * @param request the current Solr request.
+	 * @return the content type associated with this response writer.
+	 */
+	String getContentType(final SolrQueryRequest request) {
+		final HttpServletRequest httpRequest = (HttpServletRequest) request.getContext().get("httpRequest");
+		String accept = httpRequest.getHeader(HttpHeaders.ACCEPT);
+		if (accept == null) {
+			accept = "text/plain";
+		}
+		return accept;		
 	}
 }
